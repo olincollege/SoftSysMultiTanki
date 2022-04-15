55960b4b4e75dbd38d3c0dc269c4298ad24ad05e
Melissa Kazazic <mkazazic01@gmail.com>
2022-04-15 04:59:03 -0400

    continue on learning network code, working example

--- network/example_server.c
+++ network/example_server.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <netdb.h>
+
+//Following code referring to IP Address from geekpage
+//Rest of core code from HeadfirstC
 
 int main(int argc, char *argv[]) {
+    // replace with getnameinfo(), getaddrinfo()
+    int fd;
+	struct ifreq ifr;
+	
+	char iface[] = "wlo1"; // for wifi connected
+	
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	//Get IP4 IP address
+	ifr.ifr_addr.sa_family = AF_INET;
+
+	//Copy the interface name
+	strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+	
+	//get the ip address
+	ioctl(fd, SIOCGIFADDR, &ifr);
+	
+	//display ip address
+	printf("IP address of %s - %s\n" , iface , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+	
+	close(fd);
+    
+    // Main program
     char *advice[] = {
         "Take smaller bites\r\n",
         "Go for the tight jeans. No they do NOT make you look fat.\r\n",
@@ -30,6 +62,7 @@ int main(int argc, char *argv[]) {
         perror("listen");
         return -1;
     }
+
     puts("Waiting for connection");
     while(1){
         struct sockaddr_storage client_addr;
