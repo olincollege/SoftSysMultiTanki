0b4fa1abbda8304fc38257b7c35dd9b184a9b203
Melissa Kazazic <mkazazic01@gmail.com>
2022-05-02 05:02:32 -0400

    feat: apply memcpy for recv and send

--- network/peer.c
+++ network/peer.c
@@ -93,52 +93,57 @@ int main(int argc, char const *argv[]){
         exit(EXIT_FAILURE);
     }
 
+    //TO DO: organize by tank, apply to input and server
+
+    // this is good reference for the packet
+    int keys_buf_size = 18;
+    // for keyboard
+    // unpressed
+    uint8_t keyboard_up_type = 0;
+    uint16_t keyboard_up_data = 0; // which key
+    // pressed
+    uint8_t keyboard_down_type = 1;
+    uint16_t keyboard_down_data = 0; // which key
+
+    // for mouse button
+    uint8_t mouse_button_type = 2;
+    uint8_t mouse_button_data = 0; //1 for pressed
+
+    // for mouse state
+    // x
+    uint8_t mouse_state_x_type = 3;
+    uint32_t mouse_state_x_data = 0;
+    // y
+    uint8_t mouse_state_y_type = 4;
+    uint32_t mouse_state_y_data = 0;
+
     // send code
     // TO DO: to be placed within input code
     if (fork()==0){
-        // this is good reference for the packet
-        int keys_buf_size = 18;
+        // setup packet
         char keys_buffer[keys_buf_size];
-
-        // for keyboard
-        // unpressed
-        uint8_t keyboard_up_type = 0;
-        uint16_t keyboard_up_data = 0; // which key
-        // pressed
-        uint8_t keyboard_down_type = 1;
-        uint16_t keyboard_down_data = 0; // which key
-
-        // for mouse button
-        uint8_t mouse_button_type = 2;
-        uint8_t mouse_button_data = 0; //1 for pressed
-
-        // for mouse state
-        // x
-        uint8_t mouse_state_x_type = 3;
-        uint32_t mouse_state_x_data = 0;
-        // y
-        uint8_t mouse_state_y_type = 4;
-        uint32_t mouse_state_y_data = 0;
 
         // make sure to update data with input code
 
-        // put into buffer. draft for now
-        keys_buffer[0] = (char)keyboard_up_type;   //uint8_t
-        keys_buffer[1] = (char)keyboard_up_data;   //uint16_t
-        keys_buffer[3] = (char)keyboard_down_type; //uint8_t
-        keys_buffer[4] = (char)keyboard_down_data; //uint16_t
-        keys_buffer[6] = (char)mouse_button_type;  //uint8_t
-        keys_buffer[7] = (char)mouse_button_data;  //uint8_t
-        keys_buffer[8] = (char)mouse_state_x_type; //uint8_t
-        keys_buffer[9] = (char)mouse_state_x_data; //uint32_t
-        keys_buffer[13] = (char)mouse_state_y_type; //uint8_t
-        keys_buffer[14] = (char)mouse_state_y_data; //uint32_t
+        // put into buffer
+        memcpy(&keys_buffer[0],&keyboard_up_type,1); //uint8_t
+        memcpy(&keys_buffer[1],&keyboard_up_data,2); //uint16_t
+        memcpy(&keys_buffer[3],&keyboard_down_type,1); //uint8_t
+        memcpy(&keys_buffer[4],&keyboard_down_data,2); //uint16_t
+        memcpy(&keys_buffer[6],&mouse_button_type,1); //uint8_t
+        memcpy(&keys_buffer[7],&mouse_button_data,1); //uint8_t
+        memcpy(&keys_buffer[8],&mouse_state_x_type,1); //uint8_t 
+        memcpy(&keys_buffer[9],&mouse_state_x_data,4); //uint32_t
+        memcpy(&keys_buffer[13],&mouse_state_y_type,1); //uint8_t 
+        memcpy(&keys_buffer[14],&mouse_state_y_data,4); //uint32_t
 
         // send packet
-        if (send(peer_sock, keys_buffer, keys_buf_size, 0) == -1){
+        if (send(peer_sock, &keys_buffer, keys_buf_size, 0) == -1){
             perror("Can't read from host");
             exit(EXIT_FAILURE);
         }
+
+        // LOOP AROUND THIS
     }
     
     // receive code
@@ -153,6 +158,18 @@ int main(int argc, char const *argv[]){
             exit(EXIT_FAILURE);
         }
         // parse packet
+        memcpy(&keyboard_up_type,&keys_buffer[0],1); //uint8_t
+        memcpy(&keyboard_up_data,&keys_buffer[1],2); //uint16_t
+        memcpy(&keyboard_down_type,&keys_buffer[3],1); //uint8_t
+        memcpy(&keyboard_down_data,&keys_buffer[4],2); //uint16_t
+        memcpy(&mouse_button_type,&keys_buffer[6],1); //uint8_t
+        memcpy(&mouse_button_data,&keys_buffer[7],1); //uint8_t
+        memcpy(&mouse_state_x_type,&keys_buffer[8],1); //uint8_t 
+        memcpy(&mouse_state_x_data,&keys_buffer[9],4); //uint32_t
+        memcpy(&mouse_state_y_type,&keys_buffer[13],1); //uint8_t 
+        memcpy(&mouse_state_y_data,&keys_buffer[14],4); //uint32_t
+
+        // LOOP AROUND THIS
     }
     atexit(close(peer_sock));
 }
